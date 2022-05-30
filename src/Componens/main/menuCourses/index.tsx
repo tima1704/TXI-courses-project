4a64cd76c047492c4c00a0bf772ad14c4cfb237d
Александр Шайковский <saden201@gmail.com>
2022-05-30 22:05:15 +0300

    Fix: course menu

--- src/Componens/main/menuCourses/index.tsx
+++ src/Componens/main/menuCourses/index.tsx
@@ -9,34 +9,42 @@ import styles from "./index.module.css";
 export const MenuCourses: FC = () => {
   const isAuth = useAppSelector((state) => state.App.isAuth);
 
+  const onClickOpenRegisterPopUp = () => {};
+
   return (
     <div className="container">
       <div className={styles["menu"]}>
         <div className={styles["leftInfo"]}>
           <div className={styles["title"]}>Онлайн обучение</div>
-          {isAuth && (
-            <>
-              <NavLink
-                to={URL_HOME}
-                className={({ isActive }) =>
-                  classNames(styles["menuItems"], styles["menuLink"], {
-                    [styles["activeMenu"]]: isActive,
-                  })
-                }
-              >
-                Все программы
-              </NavLink>
-              <NavLink
-                to={URL_USER_COURSE}
-                className={({ isActive }) =>
-                  classNames(styles["menuItems"], styles["menuLink"], {
-                    [styles["activeMenu"]]: isActive,
-                  })
-                }
-              >
-                Ваши курсы
-              </NavLink>
-            </>
+
+          <NavLink
+            to={URL_HOME}
+            className={({ isActive }) =>
+              classNames(styles["menuItems"], styles["menuLink"], {
+                [styles["activeMenu"]]: isActive,
+              })
+            }
+          >
+            Все программы
+          </NavLink>
+          {isAuth ? (
+            <NavLink
+              to={URL_USER_COURSE}
+              className={({ isActive }) =>
+                classNames(styles["menuItems"], styles["menuLink"], {
+                  [styles["activeMenu"]]: isActive,
+                })
+              }
+            >
+              Ваши курсы
+            </NavLink>
+          ) : (
+            <span
+              className={classNames(styles["menuItems"], styles["menuLink"])}
+              onClick={onClickOpenRegisterPopUp}
+            >
+              Ваши курсы
+            </span>
           )}
         </div>
         <NavLink
