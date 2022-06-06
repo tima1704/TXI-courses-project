b77bb484ef240cd69c247ba4b61d77e131a56261
Александр Шайковский <saden201@gmail.com>
2022-06-06 16:09:14 +0300

    Add: pages index

---
+++ src/Pages/index.ts
@@ -0,0 +1,13 @@
+import { CourceItemPage } from "./CourceItem";
+import { CourcesListPage } from "./CourcesList";
+import { CourceUserItem } from "./CourceUserItem";
+import { SupportPage } from "./SupportPages";
+import { UserCourceList } from "./UserCourceList";
+
+export {
+  SupportPage,
+  CourceItemPage,
+  CourcesListPage,
+  UserCourceList,
+  CourceUserItem,
+};
