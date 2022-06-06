3f44573533cabb8f75934fb5b2f9937968d9c9e3
Александр Шайковский <saden201@gmail.com>
2022-06-06 23:37:56 +0300

    Add:  transactions statuses

--- src/Componens/Transations/index.tsx
+++ src/Componens/Transations/index.tsx
@@ -1,6 +1,10 @@
+import classNames from "classnames";
+import { Icon } from "Componens/common/Icon";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { TTransactionsStatus } from "Types/transactions";
+
+import styles from "./index.module.css";
 
 interface TransactionStatusProps {
   status: TTransactionsStatus;
@@ -9,12 +13,37 @@ interface TransactionStatusProps {
 export const TransactionStatus: FC<TransactionStatusProps> = ({ status }) => {
   const { t } = useTranslation();
   if (status === "paid") {
-    return <div>{t("paid")}</div>;
+    return (
+      <div className={classNames(styles["status"], styles["paid"])}>
+        {t("common.transactionsStatus.paid")}
+        <div>
+          <Icon icon={"success"} />
+        </div>
+      </div>
+    );
   }
 
-  if (status === "processing") {
-    return <div>{t("processing")}</div>;
+  if (status === "reject") {
+    return <div>{t("common.transactionsStatus.rejected")}</div>;
   }
 
-  return <div>{t("rejected")}</div>;
+  if (status === "fail") {
+    return (
+      <div className={classNames(styles["status"], styles["fail"])}>
+        {t("common.transactionsStatus.fail")}
+        <div>
+          <Icon icon={"error"} />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classNames(styles["status"], styles["processing"])}>
+      {t("common.transactionsStatus.processing")}
+      <div>
+        <Icon icon={"spinner"} />
+      </div>
+    </div>
+  );
 };
