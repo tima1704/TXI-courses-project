e41b83ed926ff2fb409888ff25b78d51d12164d5
Александр Шайковский <saden201@gmail.com>
2022-06-06 16:12:32 +0300

    Fix: support page

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,7 +1,7 @@
 import { CourceItemPage } from "./CourceItem";
 import { CourcesListPage } from "./CourcesList";
 import { CourceUserItem } from "./CourceUserItem";
-import { SupportPage } from "./SupportPages";
+import { SupportPage } from "./SupportPage";
 import { UserCourceList } from "./UserCourceList";
 
 export {
