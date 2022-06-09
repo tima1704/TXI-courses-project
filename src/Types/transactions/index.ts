0691b168061f3cf5181df166d15571d15755bfff
Timur Ibatullin <ibatullintima@gmail.com>
2022-06-09 19:49:45 +0600

    add: currency symbols

--- src/Types/transactions/index.ts
+++ src/Types/transactions/index.ts
@@ -1,8 +1,8 @@
 export type TTransactionsStatus = "paid" | "processing" | "reject" | "fail";
-
+export type TTransactionsCurrency = "RUB" | "EUR" | "USD" | "GBP" | "UAH" | "BYN" | "KZT" | "AZN" | "CHF" | "CZK" | "CAD" | "PLN" | "SEK" | "TRY" | "CNY" | "INR" | "BRL" | "ZAR" | "UZS" | "BGN" | "RON" | "AUD" | "HKD" | "GEL" | "KGS" | "AMD" | "AED";
 export interface ITransaction {
   courseId: number;
-  coursePrice?: { sum: string; currency: string };
+  coursePrice?: { sum: string; currency: TTransactionsCurrency };
   createdAt: string;
   expirationDate: string;
   id: number;
