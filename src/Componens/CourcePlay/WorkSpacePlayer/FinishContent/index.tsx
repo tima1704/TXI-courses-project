5a2f03bbdfe24ac5902a8fcf58f1d6e9903e6b07
Timur Ibatullin <ibatullintima@gmail.com>
2022-06-14 18:57:21 +0600

    feature/finish-text

--- src/Componens/CourcePlay/WorkSpacePlayer/FinishContent/index.tsx
+++ src/Componens/CourcePlay/WorkSpacePlayer/FinishContent/index.tsx
@@ -1,5 +1,20 @@
+import { Button } from "Componens/common/Button";
+import { LinkTo } from "Componens/common/Links";
+import { URL_HOME } from "Constants/URL";
 import { FC } from "react";
-
+import styles from "./index.module.css";
 export const FinishContent: FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles["finish_content"]}>
+      <div className={styles["body_content"]}>
+        <h3>Поздравляем!</h3>
+        <p>Поздравляем с прохождением курса.
+          Доступ к видео у вас остаётся навсегда, чтобы вы могли в любой момент освежить память.</p>
+        <span>Так-же, дарим вам 30% скидки на любоый новый курс!</span>
+      </div>
+      <LinkTo link={URL_HOME}>
+        <Button type="button">К списку курсов</Button>
+      </LinkTo>
+    </div>
+  )
 };
