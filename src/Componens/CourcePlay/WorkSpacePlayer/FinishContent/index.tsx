a94a5f0c50459b46395cce0d7b2b1a5502e6b40e
Timur Ibatullin <ibatullintima@gmail.com>
2022-06-14 19:59:28 +0600

    Add: langs in finish coureces

--- src/Componens/CourcePlay/WorkSpacePlayer/FinishContent/index.tsx
+++ src/Componens/CourcePlay/WorkSpacePlayer/FinishContent/index.tsx
@@ -3,17 +3,17 @@ import { LinkTo } from "Componens/common/Links";
 import { URL_HOME } from "Constants/URL";
 import { FC } from "react";
 import styles from "./index.module.css";
+import { t } from "i18next";
 export const FinishContent: FC = () => {
   return (
     <div className={styles["finish_content"]}>
       <div className={styles["body_content"]}>
-        <h3>Поздравляем!</h3>
-        <p>Поздравляем с прохождением курса.
-          Доступ к видео у вас остаётся навсегда, чтобы вы могли в любой момент освежить память.</p>
-        <span>Так-же, дарим вам 30% скидки на любоый новый курс!</span>
+        <h3>{t("CourcePlay.finish.title")}</h3>
+        <p>{t("CourcePlay.finish.text")}</p>
+        <span>{t("CourcePlay.finish.discount")}</span>
       </div>
       <LinkTo link={URL_HOME}>
-        <Button type="button">К списку курсов</Button>
+        <Button type="button">{t("CourcePlay.finish.backToCourses")}</Button>
       </LinkTo>
     </div>
   )
